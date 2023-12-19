98fb2f41876a8cb9db3cbc148c0f0dc7860df0b5
Ryndina Olena <ryndinaolena@gmail.com>
2023-12-19 14:09:37 +0200

    xs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,16 @@ import { PiSketchLogoThin } from "react-icons/pi";
 
 const Footer = () => {
     return (
-        <Container fluid style={{ backgroundColor: '#212529', color: 'white', display: 'flex', justifyContent: 'center', }}>
+        <Container className="float-sm-start" fluid style={{ backgroundColor: '#212529', color: 'white', display: 'flex', justifyContent: 'center', }}>
             <Row style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
                 <Col><h6 >Місто Миколаїв</h6></Col>
                 <Col><kbd>+380668382582</kbd></Col>
                 <Col><kbd>+380939331160</kbd></Col>
-                <PiSketchLogoThin style={{ fontSize: '100' }} />
+                <PiSketchLogoThin style={{ fontSize: '100' }} xs={{ fontSize: '50' }} />
             </Row>
         </Container >
     )
 }
+// className = 'd-none d-lg-block d-md-block'
 
 export default Footer
--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -10,9 +10,9 @@ const Navibar = () => {
             <Navbar bg="dark" data-bs-theme="dark">
 
                 <Navbar.Brand style={{ paddingLeft: ' 20px' }}><h3>Comfort</h3></Navbar.Brand>
-                <Container >
-                    <Nav style={{ marginRight: '100px' }} >
-                        <NavLink to="/" style={{ 'textDecoration': 'none', marginRight: '0px', color: 'white', padding: '0 20px 0 50px' }}><h6 >ГОЛОВНА</h6></NavLink>
+                <Container className="float-sm-start" sm={{ width: '540px' }} fluid="md" >
+                    <Nav md={{ marginRight: '100px' }} >
+                        <NavLink to="/" style={{ 'textDecoration': 'none', marginRight: '0px', color: 'white', paddingRight: '10px' }}><h6 >ГОЛОВНА</h6></NavLink>
                         <NavLink to="/users" style={{ 'textDecoration': 'none', marginRight: '0px', color: 'white', padding: '0 20px 0 0px' }}><h6>ПОРФОЛІО</h6></NavLink>
                         <NavLink to="/about" style={{ 'textDecoration': 'none', marginRight: '0px', color: 'white', padding: '0 20px 0 0px' }}><h6>ПРО НАС</h6></NavLink>
 
@@ -21,12 +21,11 @@ const Navibar = () => {
                 <Nav > <Nav.Link className="d-md-none" href='tel:+380939331160' style={{ marginLeft: 'auto' }}><IoCallOutline size={20} /></Nav.Link>
                 </Nav>
 
-                <Col className='d-none d-lg-block d-md-block'>
+                <Col style={{ marginRight: '50px' }} className='d-none d-lg-block d-md-block'>
                     <p style={{ color: 'white', margin: '0 auto', fontSize: '10px' }}> Дзвоніть за номером:</p>
                     <p style={{ color: 'white', margin: '0 auto', fontSize: '10px', }}> +380939331160</p>
                     <p style={{ color: 'white', margin: '0 auto', fontSize: '10px', }}> +380668382582</p>
                 </Col>
-
             </Navbar>
         </>)
 }
